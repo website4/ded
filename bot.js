cdbb5de7d1c1190b14a80513b9720c9c8a16b4a1
website <95375355+website4@users.noreply.github.com>
2022-11-20 15:13:03 +0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,7 +2,12 @@ const { Telegraf, Markup } = require('telegraf');
 require('dotenv').config();
 
 const bot = new Telegraf(`5894677459:AAFiQTNDitVhKr5sFad3J3BWHc78iGcVW5Q`);
-bot.start((ctx) => ctx.reply('Приветушки дедушки',      
+bot.start((ctx) => ctx.reply(`Приветушки дедушки 
+
+
+
+
+https://sun9-63.userapi.com/impg/l1ZiY9vZjqtRyy_8FMgBnnh5PRrHcKJh7YHHzA/Hl34fFsXz7U.jpg?size=1200x1599&quality=95&sign=d8fae3d0cfa278df6d1c05b185a7259a&type=album`,      
     Markup.keyboard([
         ['Поздравления', 'Что желает тебе Вован'],
     ])
